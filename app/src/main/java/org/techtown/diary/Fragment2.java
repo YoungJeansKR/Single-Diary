a197699812aa4fa38f268ec286d7faf3dfa469d0
kyj950514 <kyj950514@naver.com>
2023-06-08 00:01:34 +0900

    update 3.2

--- app/src/main/java/org/techtown/diary/Fragment2.java
+++ app/src/main/java/org/techtown/diary/Fragment2.java
@@ -1,7 +1,16 @@
 package org.techtown.diary;
 
+import android.content.ContentResolver;
 import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,15 +20,22 @@ import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 
 import com.github.channguyen.rsv.RangeSliderView;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.Date;
 
 public class Fragment2 extends Fragment {
     private static final String TAG = "Fragment2";
 
     Context context;
     OnTabItemSelectedListener listener;
+    OnRequestListener requestListener;
 
     ImageView weatherIcon;
     TextView dateTextView;
@@ -27,6 +43,16 @@ public class Fragment2 extends Fragment {
 
     EditText contentsInput;
     ImageView pictureImageView;
+
+    boolean isPhotoCaptured;
+    boolean isPhotoFileSaved;
+    boolean isPhotoCanceled;
+
+    int selectedPhotoMenu;
+
+    Uri uri;
+    File file;
+    Bitmap resultPhotoBitmap;
 
     @Override
     public void onAttach(Context context) {
@@ -38,6 +64,9 @@ public class Fragment2 extends Fragment {
             listener = (OnTabItemSelectedListener) context;
         }
 
+        if (context instanceof OnRequestListener) {
+            requestListener = (OnRequestListener) context;
+        }
     }
 
     @Override
@@ -47,6 +76,7 @@ public class Fragment2 extends Fragment {
         if (context != null) {
             context = null;
             listener = null;
+            requestListener = null;
         }
     }
 
@@ -55,6 +85,11 @@ public class Fragment2 extends Fragment {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment2, container, false);
 
         initUI(rootView);
+
+        // check current location
+        if (requestListener != null) {
+            requestListener.onRequest("getCurrentLocation");
+        }
 
         return rootView;
     }
@@ -70,7 +105,11 @@ public class Fragment2 extends Fragment {
         pictureImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                if(isPhotoCaptured || isPhotoFileSaved) {
+                    showDialog(AppConstants.CONTENT_PHOTO_EX);
+                } else {
+                    showDialog(AppConstants.CONTENT_PHOTO);
+                }
             }
         });
 
@@ -116,5 +155,249 @@ public class Fragment2 extends Fragment {
         sliderView.setOnSlideListener(listener);
         sliderView.setInitialIndex(2);
     }
- 
+
+    /**
+     * ① 맑음
+     * ② 구름 조금
+     * ③ 구름 많음
+     * ④ 흐림
+     * ⑤ 비
+     * ⑥ 눈/비
+     * ⑦ 눈
+     *
+     */
+    public void setWeather(String data) {
+        if (data != null) {
+            if (data.equals("맑음")) {
+                weatherIcon.setImageResource(R.drawable.weather_1);
+            } else if (data.equals("구름 조금")) {
+                weatherIcon.setImageResource(R.drawable.weather_2);
+            } else if (data.equals("구름 많음")) {
+                weatherIcon.setImageResource(R.drawable.weather_3);
+            } else if (data.equals("흐림")) {
+                weatherIcon.setImageResource(R.drawable.weather_4);
+            } else if (data.equals("비")) {
+                weatherIcon.setImageResource(R.drawable.weather_5);
+            } else if (data.equals("눈/비")) {
+                weatherIcon.setImageResource(R.drawable.weather_6);
+            } else if (data.equals("눈")) {
+                weatherIcon.setImageResource(R.drawable.weather_7);
+            } else {
+                Log.d("Fragment2", "Unknown weather string : " + data);
+            }
+        }
+    }
+
+    public void setAddress(String data) {
+        locationTextView.setText(data);
+    }
+
+    public void setDateString(String dateString) {
+        dateTextView.setText(dateString);
+    }
+
+
+    public void showDialog(int id) {
+        AlertDialog.Builder builder = null;
+
+        switch(id) {
+
+            case AppConstants.CONTENT_PHOTO:
+                builder = new AlertDialog.Builder(context);
+
+                builder.setTitle("사진 메뉴 선택");
+                builder.setSingleChoiceItems(R.array.array_photo, 0, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        selectedPhotoMenu = whichButton;
+                    }
+                });
+                builder.setPositiveButton("선택", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        if(selectedPhotoMenu == 0 ) {
+                            showPhotoCaptureActivity();
+                        } else if(selectedPhotoMenu == 1) {
+                            showPhotoSelectionActivity();
+                        }
+                    }
+                });
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+
+                    }
+                });
+
+                break;
+
+            case AppConstants.CONTENT_PHOTO_EX:
+                builder = new AlertDialog.Builder(context);
+
+                builder.setTitle("사진 메뉴 선택");
+                builder.setSingleChoiceItems(R.array.array_photo_ex, 0, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        selectedPhotoMenu = whichButton;
+                    }
+                });
+                builder.setPositiveButton("선택", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+                        if(selectedPhotoMenu == 0) {
+                            showPhotoCaptureActivity();
+                        } else if(selectedPhotoMenu == 1) {
+                            showPhotoSelectionActivity();
+                        } else if(selectedPhotoMenu == 2) {
+                            isPhotoCanceled = true;
+                            isPhotoCaptured = false;
+
+                            pictureImageView.setImageResource(R.drawable.picture1);
+                        }
+                    }
+                });
+                builder.setNegativeButton("취소", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int whichButton) {
+
+                    }
+                });
+
+                break;
+
+            default:
+                break;
+        }
+
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void showPhotoCaptureActivity() {
+        try {
+            file = createFile();
+            if (file.exists()) {
+                file.delete();
+            }
+
+            file.createNewFile();
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        if(Build.VERSION.SDK_INT >= 24) {
+            uri = FileProvider.getUriForFile(context, BuildConfig.APPLICATION_ID, file);
+        } else {
+            uri = Uri.fromFile(file);
+        }
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+
+        startActivityForResult(intent, AppConstants.REQ_PHOTO_CAPTURE);
+
+    }
+
+    private File createFile() {
+        String filename = createFilename();
+        File outFile = new File(context.getFilesDir(), filename);
+        Log.d("Main", "File path : " + outFile.getAbsolutePath());
+
+        return outFile;
+    }
+
+    public void showPhotoSelectionActivity() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+
+        startActivityForResult(intent, AppConstants.REQ_PHOTO_SELECTION);
+    }
+
+    /**
+     * 다른 액티비티로부터의 응답 처리
+     */
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        super.onActivityResult(requestCode, resultCode, intent);
+
+        if (intent != null) {
+            switch (requestCode) {
+                case AppConstants.REQ_PHOTO_CAPTURE:  // 사진 찍는 경우
+                    Log.d(TAG, "onActivityResult() for REQ_PHOTO_CAPTURE.");
+
+                    Log.d(TAG, "resultCode : " + resultCode);
+
+                    //setPicture(file.getAbsolutePath(), 8);
+                    resultPhotoBitmap = decodeSampledBitmapFromResource(file, pictureImageView.getWidth(), pictureImageView.getHeight());
+                    pictureImageView.setImageBitmap(resultPhotoBitmap);
+
+                    break;
+
+                case AppConstants.REQ_PHOTO_SELECTION:  // 사진을 앨범에서 선택하는 경우
+                    Log.d(TAG, "onActivityResult() for REQ_PHOTO_SELECTION.");
+
+                    Uri fileUri = intent.getData();
+
+                    ContentResolver resolver = context.getContentResolver();
+
+                    try {
+                        InputStream instream = resolver.openInputStream(fileUri);
+                        resultPhotoBitmap = BitmapFactory.decodeStream(instream);
+                        pictureImageView.setImageBitmap(resultPhotoBitmap);
+
+                        instream.close();
+
+                        isPhotoCaptured = true;
+                    } catch(Exception e) {
+                        e.printStackTrace();
+                    }
+
+                    break;
+
+            }
+        }
+    }
+
+    public static Bitmap decodeSampledBitmapFromResource(File res, int reqWidth, int reqHeight) {
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(res.getAbsolutePath(),options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        return BitmapFactory.decodeFile(res.getAbsolutePath(),options);
+    }
+
+    public static int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+        // Raw height and width of image
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+
+            final int halfHeight = height;
+            final int halfWidth = width;
+
+            // Calculate the largest inSampleSize value that is a power of 2 and keeps both
+            // height and width larger than the requested height and width.
+            while ((halfHeight / inSampleSize) >= reqHeight
+                    && (halfWidth / inSampleSize) >= reqWidth) {
+                inSampleSize *= 2;
+            }
+        }
+
+        return inSampleSize;
+    }
+
+
+    private String createFilename() {
+        Date curDate = new Date();
+        String curDateStr = String.valueOf(curDate.getTime());
+
+        return curDateStr;
+    }
+
+
 }
